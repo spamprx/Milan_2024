b9f7ea0450bd34422ab35a95cc24b28e32a2848b
Tejasri-N <145594007+Tejasri-N@users.noreply.github.com>
2024-06-21 17:10:38 +0530

    Create Table.jsx in components

---
+++ src/components/Table.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import DataTable from "react-data-table-component";
+
+function Table({ games, blocknames, points }) {
+  const headers = [
+    {
+      name: "S.No",
+      selector: (row) => row.num,
+      sortable: true,
+    },
+    {
+      name: "Sport",
+      selector: (row) => row.sport,
+      sortable: true,
+    },
+    ...blocknames.map((block) => ({
+      name: block,
+      selector: (row) => row[block],
+      sortable: true,
+    })),
+  ];
+
+  const data = games.map((game, gameIndex) => {
+    const rowData = {
+      num: gameIndex + 1,
+      sport: game,
+    };
+    blocknames.forEach((block, blockIndex) => {
+      rowData[block] = points[gameIndex + 1][blockIndex];
+    });
+    return rowData;
+  });
+
+  return <DataTable columns={headers} data={data} fixedHeader pagination />;
+}
+
+export default Table;
