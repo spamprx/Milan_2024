4963e9c54248c827e7a8c499e50a630ae6071215
siddu0660 <sathwik17062006@gmail.com>
2024-08-25 00:48:52 +0530

    Livescore Cards

---
+++ client/src/components/CardLiveScoreRev.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import Subtract from "../assets/SubtractRev.svg";
+import Basketball from "../assets/Basketball.png";
+
+function CardLiveScoreRev() {
+  return (
+    <div className="relative">
+      <div className="relative flex w-fit bg-gradient-to-r from-[#D9d9d9] to-gray-400 rounded-lg">
+        <img
+          src={Subtract}
+          alt="Subtract"
+          className="flex-1 translate-x-6 translate-y-3 z-20"
+        />
+        <div className="absolute inset-0 flex items-center -translate-x-3 -translate-y-3 justify-center z-20 ">
+          <div className="flex flex-col -translate-y-2 gap-2 -translate-x-7">
+            <div className="bg-[#4B16B2] text-xl font-bold font-be-vietnam text-white  ring-[#4B16B2] ring-1 -translate-y-32 p-2 rounded-tl-xl rounded-br-xl z-20">
+              LIVESCORE
+            </div>
+            <div className="text-md font-bold font-be-vietnam text-white  -translate-y-16 p-2 rounded-tr-xl rounded-bl-xl z-20">
+              MATCH : 09
+            </div>
+          </div>
+          <div className="translate-x-6 -translate-y-40 p-2 rounded-tr-xl rounded-bl-xl z-20">
+            <div className="flex flex-col text-xs font-be-vietnam text-[#4B16B2]">
+              TIME
+              <span className="text-xl font-extrabold -translate-y-2">
+                00:00
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="absolute inset-0 flex flex-col items-center -translate-x-20 translate-y-8 justify-center">
+          <div className="bg-[#170734]/[0.57] w-1/4 h-1/4 z-10"></div>
+          <div className="bg-[#170734]/[0.57] w-1/4 h-1/2 z-10"></div>
+          <img
+            src={Basketball}
+            alt="Basketball"
+            className="absolute w-1/2 -translate-x-20"
+          />
+        </div>
+        <div className="flex flex-row absolute inset-0 items-center justify-center z-50 translate-x-20 -translate-y-12">
+          <div className="flex flex-col translate-x-1 gap-2">
+            <span className="text-4xl font-bold font-be-vietnam text-[#4B16B2] -translate-y-2">
+              00
+            </span>
+            <span className="text-md font-be-vietnam text-[#F3F1F6]">
+              Hostel 1
+            </span>
+            <hr className="w-16 border-1 border-[#F3F1F6]" />
+            <span className="text-ld font-bold font-be-vietnam text-[#F3F1F6]">
+              00
+            </span>
+          </div>
+          <div className="flex flex-col -translate-x-2 gap-3">
+            <span className="text-4xl font-bold font-be-vietnam text-white mb-6 -translate-y-2">
+              :
+            </span>
+            <hr className="w-16 border-1 border-[#F3F1F6] translate-y-1" />
+            <span className="text-xs font-semibold font-be-vietnam bg-[#F3F1F6] text-[#4B16B2] rounded-2xl my-1">
+              Fouls
+            </span>
+          </div>
+          <div className="flex flex-col -translate-x-5 gap-2">
+            <span className="text-4xl font-bold font-be-vietnam text-[#4B16B2] -translate-y-2">
+              00
+            </span>
+            <span className="text-md font-be-vietnam text-[#F3F1F6]">
+              Hostel 2
+            </span>
+            <hr className="w-16 border-1 border-[#F3F1F6]" />
+            <span className="text-ld font-bold font-be-vietnam text-[#F3F1F6]">
+              00
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardLiveScoreRev;
