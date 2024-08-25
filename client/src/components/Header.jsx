1e4cd888ffcffa537231c273fde7f1251442d80c
siddu0660 <sathwik17062006@gmail.com>
2024-08-26 03:47:13 +0530

    Header breakpoints

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
 
     useEffect(() => {
         const handleResize = () => {
-            setIsSmallScreen(window.innerWidth < 860);
+            setIsSmallScreen(window.innerWidth < 870);
         };
 
         handleResize();
