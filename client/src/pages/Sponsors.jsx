94eaf1dc69d624d633ff0690aa781690490fbcfe
Tejasri <ep23btech11018@iith.ac.in>
2024-08-27 00:38:57 +0530

    Updated Sponsors

--- client/src/pages/Sponsors.jsx
+++ client/src/pages/Sponsors.jsx
@@ -5,19 +5,93 @@ function Sponsors() {
   return (
     // <h1>Work under Progress.......</h1>
     <div className="relative w-full h-screen">
-      <div className="w-full h-1/5 bg-[#140432]"></div>
+      <div className="flex items-center justify-center w-full h-1/6 bg-[#140432]">
+        <div className="flex w-1/3 h-1/2 justify-center items-center rounded-3xl bg-[#6B5794] text-2xl font-bold text-white">
+          LOREM IPSUM
+        </div>
+      </div>
 
       <div
-        className="w-full h-full bg-cover bg-center"
+        className="flex flex-col w-full h-full bg-cover bg-center"
         style={{ backgroundImage: `url(${SponsorBg})` }}
-      ></div>
+      >
+        <div className="flex flex-row justify-between items-center bg-transparent w-full h-full rounded-2xl">
+          <div className="w-1/2 h-2/3 rounded-2xl m-5">
+            <img
+              src={SponsorBg2}
+              alt="Sponsor1"
+              className="rounded-2xl w-full h-full"
+            />
+          </div>
+          <div className="w-1/2 h-2/3 rounded-2xl m-5">
+            <img
+              src={SponsorBg2}
+              alt="Sponsor1"
+              className="rounded-2xl w-full h-full"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row bg-transparent w-full h-full rounded-2xl">
+          <div className="w-1/2 h-2/3 rounded-2xl m-5">
+            <img
+              src={SponsorBg2}
+              alt="Sponsor1"
+              className="rounded-2xl w-full h-full"
+            />
+          </div>
+          <div className="w-1/2 h-2/3 rounded-2xl m-5">
+            <img
+              src={SponsorBg2}
+              alt="Sponsor1"
+              className="rounded-2xl w-full h-full"
+            />
+          </div>
+        </div>
+      </div>
 
-      <div className="w-full h-1/5 bg-[#140432]"></div>
+      <div className="flex items-center justify-center w-full h-1/5 bg-[#140432]">
+        <div className="flex w-1/3 h-1/2 justify-center items-center rounded-3xl bg-[#6B5794] text-2xl font-bold text-white">
+          LOREM IPSUM
+        </div>
+      </div>
 
       <div
-        className="w-full h-full bg-cover bg-center"
+        className="flex flex-col w-full h-full bg-cover bg-center"
         style={{ backgroundImage: `url(${SponsorBg2})` }}
-      ></div>
+      >
+        <div className="flex flex-row justify-between items-center bg-transparent w-full h-full rounded-2xl">
+          <div className="w-1/2 h-2/3 rounded-2xl m-5">
+            <img
+              src={SponsorBg}
+              alt="Sponsor1"
+              className="rounded-2xl w-full h-full"
+            />
+          </div>
+          <div className="w-1/2 h-2/3 rounded-2xl m-5">
+            <img
+              src={SponsorBg}
+              alt="Sponsor1"
+              className="rounded-2xl w-full h-full"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row bg-transparent w-full h-full rounded-2xl">
+          <div className="w-1/2 h-2/3 rounded-2xl m-5">
+            <img
+              src={SponsorBg}
+              alt="Sponsor1"
+              className="rounded-2xl w-full h-full"
+            />
+          </div>
+          <div className="w-1/2 h-2/3 rounded-2xl m-5">
+            <img
+              src={SponsorBg}
+              alt="Sponsor1"
+              className="rounded-2xl w-full h-full"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
