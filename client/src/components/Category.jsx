fa81542a89e922e17d3a4c5f15bc5711d7e14b53
siddu0660 <sathwik17062006@gmail.com>
2024-09-11 12:23:11 +0530

    Updated Events

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -1,106 +1,28 @@
-import { useState , useEffect } from "react";
+import React, { useState } from "react";
+import Filter from "./CategoryFilter";
 import DivBar from "./Graph";
-import GraphMobile from "./GraphMobile";
 import Table from "./Table";
 
-function ShowSelect({ param, onSelect }) {
-  const [selectedOptions, setSelectedOptions] = useState([]);
-  const handleCheckboxChange = (option) => {
-    setSelectedOptions((prevSelected) => {
-      if (prevSelected.includes(option)) {
-        return prevSelected.filter((item) => item !== option);
-      } else {
-        return [...prevSelected, option];
-      }
-    });
-  };
-
-  const handleSelectAll = () => {
-    if (selectedOptions.length === param.length) {
-      setSelectedOptions([]);
-    } else {
-      setSelectedOptions([...param]);
-    }
-  };
-
-  useEffect(() => {
-    onSelect(selectedOptions);
-  }, [selectedOptions, onSelect]);
-
+function Filters({ blocks, games, filteredBlocks, filteredGames }) {
   return (
-    <div className="block-select text-start pl-4">
-      <div>
-        <label>
-          <input
-            type="checkbox"
-            checked={selectedOptions.length === param.length}
-            onChange={handleSelectAll}
-          />
-          Select All
-        </label>
-      </div>
-      <ul>
-        {param.map((option, index) => (
-          <li key={index}>
-            <label>
-              <input
-                type="checkbox"
-                value={option}
-                checked={selectedOptions.includes(option)}
-                onChange={() => handleCheckboxChange(option)}
-              />
-              {option}
-            </label>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
-
-export function Filter({ blocks, games, filteredBlocks, filteredGames }) {
-  const [showBlocks, setShowBlocks] = useState(false);
-  const [showGames, setShowGames] = useState(false);
-  const [selectedBlocks, setSelectedBlocks] = useState([...blocks]);
-  const [selectedGames, setSelectedGames] = useState([...games]);
-
-  const handleSave = () => {
-    filteredBlocks(selectedBlocks);
-    filteredGames(selectedGames);
-    setShowBlocks(false);
-    setShowGames(false);
-  };
-
-  return (
-    <div className="text-white w-64 bg-gradient-to-r from-purple-500 to-purple-700 font-medium rounded-lg text-md py-2.5 text-start px-10">
-      Available Options -
-      <button className="pl-2" onClick={() => setShowBlocks(!showBlocks)}>
-        Select Blocks :
-      </button>
-      {showBlocks && <ShowSelect param={blocks} onSelect={setSelectedBlocks} />}
-      <button className="pl-2" onClick={() => setShowGames(!showGames)}>
-        Select Sports :
-      </button>
-      {showGames && <ShowSelect param={games} onSelect={setSelectedGames} />}
-      <br />
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
-        onClick={handleSave}
-      >
-        Save
-      </button>
+    <div className="flex flex-row gap-[30vw] justify-center">
+      <Filter
+        options={blocks}
+        onCategoryChange={filteredBlocks}
+        title="Select Block"
+      />
+      <Filter
+        options={games}
+        onCategoryChange={filteredGames}
+        title="Select Event"
+      />
     </div>
   );
 }
 
 function Category({ title, blocknames, games, points }) {
-  const [isFiltered, setIsFiltered] = useState(false);
   const [filteredBlocks, setFilteredBlocks] = useState(blocknames);
   const [filteredGames, setFilteredGames] = useState(games);
-
-  const handleFilter = () => {
-    setIsFiltered(!isFiltered);
-  };
 
   const handleFilteredBlocks = (selectedBlocks) => {
     setFilteredBlocks(selectedBlocks);
@@ -119,37 +41,28 @@ function Category({ title, blocknames, games, points }) {
   });
 
   return (
-    <>
+    <div className="flex flex-col items-center">
+      <Filters
+        blocks={blocknames}
+        games={games}
+        filteredBlocks={handleFilteredBlocks}
+        filteredGames={handleFilteredGames}
+      />
       <DivBar
-        blocknames={isFiltered ? filteredBlocks : blocknames}
-        games={isFiltered ? filteredGames : games}
-        points={isFiltered ? filteredPoints : points}
+        blocknames={filteredBlocks}
+        games={filteredGames}
+        points={filteredPoints}
         title={title}
       />
-      <button
-        type="button"
-        className="text-white bg-gradient-to-r from-purple-500 to-purple-700 font-medium rounded-lg text-md py-2.5 text-center px-10"
-        onClick={handleFilter}
-      >
-        {isFiltered ? "Show All" : "Filter"}
-      </button>
-      {isFiltered && (
-        <Filter
-          blocks={blocknames}
-          games={games}
-          filteredBlocks={handleFilteredBlocks}
-          filteredGames={handleFilteredGames}
-        />
-      )}
-      <div className="table mx-auto py-8 px-4">
+      <div className="table mx-auto py-8 px-4 w-full">
         <Table
-          blocknames={isFiltered ? filteredBlocks : blocknames}
-          games={isFiltered ? filteredGames : games}
-          points={isFiltered ? filteredPoints : points}
+          blocknames={filteredBlocks}
+          games={filteredGames}
+          points={filteredPoints}
           tag={title}
         />
       </div>
-    </>
+    </div>
   );
 }
 
