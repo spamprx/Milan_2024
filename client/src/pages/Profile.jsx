f404a10b440ff3aec5a271b7495b6c63ace9b4d5
spamprx <cs23btech11012@iith.ac.in>
2024-09-13 00:55:55 +0530

    Update

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -42,7 +42,7 @@ const Profile = () => {
             label: event,
           }))
         );
-        setAuth(true); // Set auth to true upon successful authentication
+        setAuth(true);
       })
       .catch((error) => {
         console.error("Error fetching user details: ", error);
@@ -163,7 +163,6 @@ const Profile = () => {
     </div>
   );
 
-  
   const customStyles = {
     control: (provided) => ({
       ...provided,
@@ -241,7 +240,7 @@ const Profile = () => {
   const SubmitButton = () => (
     <div className="flex items-center justify-center mb-4">
       <button
-        className="bg-[#8F33BA] text-[#D1CCB6] font-[700] font-montserrat py-4 px-20 rounded-xl focus:outline-none focus:shadow-outline mb-8 mt-6"
+        className="bg-[#8F33BA] text-[#D1CCB6] font-[700] font-montserrat py-4 px-20 rounded-xl focus:outline-none focus:shadow-outline mb-8 mt-6 hover:bg-[#7a2a9e]"
         type="submit"
       >
         Sign Up
@@ -254,130 +253,134 @@ const Profile = () => {
   }
 
   return (
-      <>
-        {!auth && (
-          <div className="container mx-auto flex justify-center items-center min-h-[50vh]">
-            <GoogleButton />
-          </div>
-        )}
-  
-        {auth && (
-          <div className="container mx-auto px-4 mt-10 mb-20">
-            <div className="bg-none shadow-lg rounded-lg overflow-hidden min-h-[600px] max-w-4xl mx-auto">
-              <div className="text-2xl py-6 px-6 text-white text-center font-[700] uppercase">
-                {!submitted ? "MILAN" : "Your Profile has been created!"}
-              </div>
-              <div
-                ref={formContainerRef}
-                className="form-container bg-[#D1CCB6] rounded-3xl w-full sm:w-3/4 md:w-1/2 lg:w-1/2 mx-auto overflow-y-auto h-[calc(100%-20px)]"
-              >
-                {!submitted ? (
-                  <form
-                    className="py-4 px-6 space-y-6 rounded-2xl mx-auto"
-                    onSubmit={handleSubmit}
-                  >
-                    <InputField
-                      label="Name"
-                      id="name"
-                      type="text"
-                      placeholder="Enter your name"
-                      value={user.name}
-                      onChange={(e) => setUser({ ...user, name: e.target.value })}
-                    />
-                    <InputField
-                      label="Email"
-                      id="email"
-                      type="email"
-                      placeholder="Enter your email"
-                      value={user.email}
-                      onChange={(e) => setUser({ ...user, email: e.target.value })}
-                    />
-                    <SelectField
-                      label="Select Block:"
-                      id="block"
-                      options={blockOptions}
-                      value={user.Block}
-                      onChange={(selected) => setUser({ ...user, Block: selected })}
-                      onMenuOpen={handleSelectOpen}
-                      placeholder="Select a Block"
-                    />
-                    <SelectField
-                      label="Select Events:"
-                      id="event"
-                      options={eventOptions}
-                      value={selectedEvents}
-                      onChange={handleEventChange}
-                      onMenuOpen={handleSelectOpen}
-                      placeholder="Select Events"
-                      isMulti={true}
-                    />
-                    <SubmitButton />
-                  </form>
-                ) : (
-                  <div className="py-10 px-10 space-y-6 md:w-4/5 lg:w-3/5 mx-auto">
-                    <InputField
-                      label="Name"
-                      id="name"
-                      type="text"
-                      value={user.name}
-                      readOnly={true}
-                    />
-                    <InputField
-                      label="Email"
-                      id="email"
-                      type="email"
-                      value={user.email}
-                      readOnly={true}
-                    />
-                    <div className="mb-4">
-                      <h3 className="text-lg font-[600] text-[#1E1E1E] mb-2 text-left font-be-vietnam-pro">Selected Block:</h3>
-                      <div className="bg-gray-100 p-2 rounded">
-                        {user.Block ? user.Block.label : 'None'}
-                      </div>
-                    </div>
-                    <div className="mb-4">
-                      <h3 className="text-lg font-[600] text-[#1E1E1E] mb-2 text-left font-be-vietnam-pro">
-                        Interested In:
-                        <button
-                          onClick={toggleEditMode}
-                          className="ml-2 px-2 py-1 text-sm bg-[#8F33BA] text-[#D1CCB6] rounded"
-                        >
-                          {isEditing ? 'Save' : 'Edit'}
-                        </button>
-                      </h3>
-                      {isEditing ? (
-                        <SelectField
-                          id="event"
-                          options={eventOptions}
-                          value={selectedEvents}
-                          onChange={handleEventChange}
-                          onMenuOpen={handleSelectOpen}
-                          placeholder="Select Events"
-                          isMulti={true}
-                        />
-                      ) : (
-                        <div className="bg-gray-100 p-2 mb-10 rounded">
-                          {selectedEvents.length > 0 ? (
-                            <ul className="list-disc list-inside bg">
-                              {selectedEvents.map(event => (
-                                <li key={event.value}>{event.label}</li>
-                              ))}
-                            </ul>
-                          ) : (
-                            'None'
-                          )}
-                        </div>
-                      )}
+    <>
+      {!auth && (
+        <div className="container mx-auto flex justify-center items-center min-h-[50vh]">
+          <GoogleButton />
+        </div>
+      )}
+
+      {auth && (
+        <div className="container mx-auto px-4 mt-10 mb-20">
+          <div className="bg-none shadow-lg rounded-lg overflow-hidden min-h-[600px] max-w-4xl mx-auto">
+            <div className="text-2xl py-6 px-6 text-white text-center font-[700] uppercase">
+              {!submitted ? "MILAN" : "Your Profile has been created!"}
+            </div>
+            <div
+              ref={formContainerRef}
+              className="form-container bg-[#D1CCB6] rounded-3xl w-full sm:w-3/4 md:w-1/2 lg:w-1/2 mx-auto overflow-y-auto h-[calc(100%-20px)]"
+            >
+              {!submitted ? (
+                <form
+                  className="py-4 px-6 space-y-6 rounded-2xl mx-auto"
+                  onSubmit={handleSubmit}
+                >
+                  <InputField
+                    label="Name"
+                    id="name"
+                    type="text"
+                    placeholder="Enter your name"
+                    value={user.name}
+                    onChange={(e) => setUser({ ...user, name: e.target.value })}
+                  />
+                  <InputField
+                    label="Email"
+                    id="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    value={user.email}
+                    onChange={(e) =>
+                      setUser({ ...user, email: e.target.value })
+                    }
+                  />
+                  <SelectField
+                    label="Select Block:"
+                    id="block"
+                    options={blockOptions}
+                    value={user.Block}
+                    onChange={(selected) => setUser({ ...user, Block: selected })}
+                    onMenuOpen={handleSelectOpen}
+                    placeholder="Select a Block"
+                  />
+                  <SelectField
+                    label="Select Events:"
+                    id="event"
+                    options={eventOptions}
+                    value={selectedEvents}
+                    onChange={handleEventChange}
+                    onMenuOpen={handleSelectOpen}
+                    placeholder="Select Events"
+                    isMulti={true}
+                  />
+                  <SubmitButton />
+                </form>
+              ) : (
+                <div className="space-y-6 md:w-4/5 lg:w-3/5 mx-auto">
+                  <InputField
+                    label="Name"
+                    id="name"
+                    type="text"
+                    value={user.name}
+                    readOnly={true}
+                  />
+                  <InputField
+                    label="Email"
+                    id="email"
+                    type="email"
+                    value={user.email}
+                    readOnly={true}
+                  />
+                  <div className="mb-4">
+                    <h3 className="text-lg font-[600] text-[#1E1E1E] mb-2 text-left font-be-vietnam-pro">
+                      Selected Block:
+                    </h3>
+                    <div className="bg-gray-100 p-2 rounded">
+                      {user.Block ? user.Block.label : "None"}
                     </div>
                   </div>
-                )}
-              </div>
+                  <div className="mb-4">
+                    <h3 className="text-lg font-[600] text-[#1E1E1E] mb-2 text-left font-be-vietnam-pro">
+                      Interested In:
+                      <button
+                        onClick={toggleEditMode}
+                        className="ml-2 px-2 py-1 text-sm bg-[#8F33BA] text-[#D1CCB6] rounded hover:bg-[#7a2a9e]"
+                      >
+                        {isEditing ? "Save" : "Edit"}
+                      </button>
+                    </h3>
+                    {isEditing ? (
+                      <SelectField
+                        id="event"
+                        options={eventOptions}
+                        value={selectedEvents}
+                        onChange={handleEventChange}
+                        onMenuOpen={handleSelectOpen}
+                        placeholder="Select Events"
+                        isMulti={true}
+                      />
+                    ) : (
+                      <div className="bg-gray-100 p-2 mb-10 rounded">
+                        {selectedEvents.length > 0 ? (
+                          <ul className="list-disc list-inside bg">
+                            {selectedEvents.map((event) => (
+                              <li key={event.value}>{event.label}</li>
+                            ))}
+                          </ul>
+                        ) : (
+                          "None"
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              )}
             </div>
-            <ToastContainer position="bottom-right" autoClose={3000} />
           </div>
-        )}
-      </>
-    );
-  };
-  
-  export default Profile;
+          <ToastContainer position="bottom-right" autoClose={3000} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Profile;
