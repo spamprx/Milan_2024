3a06fc0ceb49ceb0bf9edf1c7a0e0abe63b31afa
spamprx <cs23btech11012@iith.ac.in>
2024-09-15 20:52:15 +0530

    Update

--- client/src/components/Eventlist.jsx
+++ client/src/components/Eventlist.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { format } from 'date-fns';
-import Meeting from './Meeting.jsx';
+import Meeting from "./Meetings"
 
 export default function EventList({ 
   showError, 
