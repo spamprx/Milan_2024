90061fe04f369bf0ac8d88bacc29761c458e1e85
manoharmaripe <manoharmaripe.19@gmail.com>
2024-09-15 12:28:20 +0530

    Update RuleBook.jsx

--- client/src/pages/RuleBook.jsx
+++ client/src/pages/RuleBook.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 function RuleBook() {
   useEffect(() => {
     if (!window.hasRedirected) {
-      window.open('https://drive.google.com/file/d/19x6wXCwdY1wUR0YByT8_KhcxGI6hLIU9/view', '_blank');
+      window.location.href = 'https://drive.google.com/file/d/19x6wXCwdY1wUR0YByT8_KhcxGI6hLIU9/view';
       window.hasRedirected = true;
     }
   }, []);
