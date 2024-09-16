186228d03177aeb058745b20e1d8c6de9a613c7f
Tejasri <ep23btech11018@iith.ac.in>
2024-09-16 22:34:35 +0530

    Updated Sponsors

--- client/src/pages/Sponsors.jsx
+++ client/src/pages/Sponsors.jsx
@@ -1,14 +1,13 @@
+import { useState, useEffect } from "react";
+import SponsorArrow from "../assets/SponsorArrow.png";
 import SponsorBg from "../assets/Sponsors.jpeg";
 import SponsorBg2 from "../assets/Sponsors2.jpeg";
-import SponsorArrow from "../assets/SponsorArrow.png";
-import { useState, useEffect } from "react";
 import Loading from "./Loading";
 
 function Sponsors() {
   const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [sponsorImages, setSponsorImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const totalImages = 12; // Total number of images to load
-  const [loadedImages, setLoadedImages] = useState(0);
 
   useEffect(() => {
     const handleResize = () => {
@@ -23,16 +22,16 @@ function Sponsors() {
     };
   }, []);
 
-  const handleImageLoad = () => {
-    setLoadedImages((prev) => prev + 1);
-  };
-
-  // If all images are loaded, set isLoading to false
   useEffect(() => {
-    if (loadedImages === totalImages) {
-      setIsLoading(false);
-    }
-  }, [loadedImages, totalImages]);
+    // fetch("/assets/sponsorImages.json");
+    fetch("/SPONSORS/Sponsors.json")
+      .then((response) => response.json())
+      .then((data) => setSponsorImages(data.sponsors))
+      .catch((error) => console.error("Error fetching JSON:", error))
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
   if (isLoading) {
     return <Loading />;
@@ -52,7 +51,6 @@ function Sponsors() {
                 src={SponsorArrow}
                 alt="SponsorArrow"
                 className="w-full scale-y-125 sm:scale-y-75 lg:scale-y-[65%]"
-                // onLoad={handleImageLoad}
               />
             </div>
 
@@ -63,58 +61,21 @@ function Sponsors() {
 
           <div
             className="flex flex-col w-full h-full bg-cover bg-center"
-            onLoad={handleImageLoad}
             style={{ backgroundImage: `url(${SponsorBg})` }}
           >
             <div className="w-full h-full flex flex-wrap justify-center items-center">
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg2}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg2}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg2}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg2}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg2}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg2}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
+              {sponsorImages.slice(0, 6).map((image, index) => (
+                <div
+                  key={index}
+                  className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5"
+                >
+                  <img
+                    src={image}
+                    alt={`Sponsor ${index + 1}`}
+                    className="rounded-2xl w-full h-full object-contain"
+                  />
+                </div>
+              ))}
             </div>
           </div>
 
@@ -124,7 +85,6 @@ function Sponsors() {
                 src={SponsorArrow}
                 alt="SponsorArrow"
                 className="w-full scale-y-125 sm:scale-y-75 lg:scale-y-[65%]"
-                // onLoad={handleImageLoad}
               />
             </div>
 
@@ -135,59 +95,21 @@ function Sponsors() {
 
           <div
             className="flex flex-col w-full h-full bg-cover bg-center"
-            onLoad={handleImageLoad}
             style={{ backgroundImage: `url(${SponsorBg2})` }}
           >
             <div className="w-full h-full flex flex-wrap justify-center items-center">
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
-              <div className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5">
-                <img
-                  src={SponsorBg}
-                  alt="Sponsor1"
-                  className="rounded-2xl w-full h-full object-contain"
-                  onLoad={handleImageLoad}
-                />
-              </div>
+              {sponsorImages.slice(6, 12).map((image, index) => (
+                <div
+                  key={index}
+                  className="w-full sm:w-1/2 lg:w-1/3 rounded-2xl p-5"
+                >
+                  <img
+                    src={image}
+                    alt={`Sponsor ${index + 7}`}
+                    className="rounded-2xl w-full h-full object-contain"
+                  />
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -195,4 +117,5 @@ function Sponsors() {
     </>
   );
 }
+
 export default Sponsors;
