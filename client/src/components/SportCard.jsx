f680dc00d9727f958019c95a63adfae2daf426bd
Tejasri <ep23btech11018@iith.ac.in>
2024-09-17 18:33:31 +0530

    Updated sport card in home

--- client/src/components/SportCard.jsx
+++ client/src/components/SportCard.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 
 const SportCard = ({ event }) => {
+  const isAllBlocks = event.teams.toLowerCase() === "all blocks";
+  const hostels = event.teams.split(",").map((hostel) => hostel.trim());
   return (
     <div className="flex justify-center items-center h-64 sm:h-80 lg:h-96 w-full px-2 sm:px-4">
       <div className="bg-[#D9D9D9] w-full h-full text-gray-500 rounded-xl shadow-lg overflow-hidden">
         <div className="bg-[#DAA827] w-full h-3/4 rounded-t-xl flex flex-row justify-between p-2 sm:p-4">
-          <div className="flex flex-col items-center w-1/3">
-            <div className="bg-[#755400]/[0.50] opacity-50 w-full h-3/4 rounded-b-xl mb-2"></div>
-            <p className="text-[#080808] text-xs sm:text-sm lg:text-lg font-semibold">
-              {event.teams}
-            </p>
-          </div>
-          <div className="flex flex-col items-center w-1/3">
-            <div className="bg-[#755400]/[0.50] opacity-50 w-full h-3/4 rounded-b-xl mb-2"></div>
-            <p className="text-[#080808] text-xs sm:text-sm lg:text-lg font-semibold">
-              {event.hostel2}
-            </p>
-          </div>
+          {isAllBlocks ? (
+            <div className="flex flex-col items-center w-full">
+              <div className="bg-[#755400]/[0.50] opacity-50 w-full h-3/4 rounded-b-xl mb-2"></div>
+              <p className="text-[#080808] text-xs sm:text-sm lg:text-lg font-semibold">
+                All Blocks
+              </p>
+            </div>
+          ) : (
+            <>
+              <div className="flex flex-col items-center w-1/3">
+                <div className="bg-[#755400]/[0.50] opacity-50 w-full h-3/4 rounded-b-xl mb-2"></div>
+                <p className="text-[#080808] text-xs sm:text-sm lg:text-lg font-semibold">
+                  {hostels[0]}
+                </p>
+              </div>
+              <div className="flex flex-col items-center w-1/3">
+                <div className="bg-[#755400]/[0.50] opacity-50 w-full h-3/4 rounded-b-xl mb-2"></div>
+                <p className="text-[#080808] text-xs sm:text-sm lg:text-lg font-semibold">
+                  {hostels[1]}
+                </p>
+              </div>
+            </>
+          )}
         </div>
         <div className="flex justify-center items-center h-1/4">
           <p className="text-[#000] text-lg sm:text-xl lg:text-3xl font-extrabold">
