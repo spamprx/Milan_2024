bd1f7f827ea2c002c47827197ddf7e1574c58440
siddu0660 <sathwik17062006@gmail.com>
2024-09-20 01:34:01 +0530

    Update

--- client/src/pages/Team.jsx
+++ client/src/pages/Team.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Image from "../assets/SponsorArrow.png";
 import TeamMember from "../components/TeamMember";
-import Milan_OC from "../../public/TEAM/OC/Milan OC.png";
+import Milan_OC from "/TEAM/OC/Milan OC.png";
 import Loading from "./Loading.jsx";
 
 function Team() {
