323e00e9ea5f5787b12c32244b520f7e9c804cf5
siddu0660 <sathwik17062006@gmail.com>
2024-09-19 16:58:55 +0530

    updated card

--- client/src/components/CardLiveScoreRev.jsx
+++ client/src/components/CardLiveScoreRev.jsx
@@ -7,6 +7,9 @@ function CardLiveScoreRev({ match }) {
   const team2 = match.team2;
   const score1 = match.score1;
   const score2 = match.score2;
+  const extra1 = match.extra1;
+  const extra2 = match.extra2;
+  const extra = match.extra;
   const ID = match.matchId.replace(/\D/g, "");
   const sportImage = `/CARD_ASSETS/${sport}.png`;
 
@@ -23,7 +26,7 @@ function CardLiveScoreRev({ match }) {
           alt="Subtract"
           className="flex-1 translate-x-6 translate-y-3 z-20"
         />
-        <div className="absolute inset-0 flex items-center -translate-x-3 -translate-y-3 justify-center z-20 ">
+        <div className="absolute inset-0 flex items-center -translate-x-3 -translate-y-3 justify-center z-20">
           <div className="flex flex-col -translate-y-2 gap-2 -translate-x-7">
             <div className="bg-[#4B16B2] text-xl font-bold font-be-vietnam text-white ring-[#4B16B2] ring-1 -translate-y-32 p-2 rounded-tl-xl rounded-br-xl z-20">
               LIVESCORE
@@ -51,37 +54,37 @@ function CardLiveScoreRev({ match }) {
           />
         </div>
         <div className="flex flex-row absolute inset-0 items-center justify-center z-30 translate-x-20 -translate-y-12">
-          <div className="flex flex-col translate-x-1 gap-2">
-            <span className="text-4xl font-bold font-be-vietnam text-[#4B16B2] text-center -translate-y-2">
+          <div className="flex w-16 flex-col translate-x-1 gap-2">
+            <span className="text-4xl font-bold font-be-vietnam text-[#4B16B2] text-center">
               {score1}
             </span>
-            <span className="text-xs font-be-vietnam w-full text-[#F3F1F6]">
+            <span className="text-xs truncate mt-3 font-be-vietnam w-full text-[#F3F1F6]">
               {team1}
             </span>
-            <hr className="w-16 border-1 border-[#F3F1F6]" />
-            <span className="text-ld font-bold font-be-vietnam text-[#F3F1F6]">
-              00
+            <hr className="w-16 border-1 border-[#F3F1F6] mb-2" />
+            <span className="text-center font-bold mt-2 font-be-vietnam text-[#F3F1F6]">
+              {extra1}
             </span>
           </div>
-          <div className="flex flex-col -translate-x-2 gap-2">
+          <div className="flex flex-col -translate-x-2 gap-2 mb-3">
             <span className="text-4xl font-bold font-be-vietnam text-white mb-6 -translate-y-2">
               :
             </span>
             <hr className="w-16 border-2 border-[#F3F1F6]" />
-            <span className="text-xs font-semibold font-be-vietnam bg-[#F3F1F6] text-[#4B16B2] rounded-2xl my-1">
-              Fouls
+            <span className="text-xs scale-110 w-fit px-1 mx-auto font-semibold font-be-vietnam bg-[#F3F1F6] text-[#4B16B2] rounded-2xl">
+              {extra}
             </span>
           </div>
-          <div className="flex flex-col -translate-x-5 gap-2">
-            <span className="text-4xl font-bold font-be-vietnam text-[#4B16B2] text-center -translate-y-2">
+          <div className="flex w-16 flex-col -translate-x-5 gap-2">
+            <span className="text-4xl font-bold font-be-vietnam text-[#4B16B2] text-center ">
               {score2}
             </span>
-            <span className="text-xs font-be-vietnam w-full text-[#F3F1F6]">
+            <span className="text-xs truncate mt-3 font-be-vietnam w-full text-[#F3F1F6]">
               {team2}
             </span>
-            <hr className="w-16 border-1 border-[#F3F1F6]" />
-            <span className="text-ld font-bold font-be-vietnam text-[#F3F1F6]">
-              00
+            <hr className="w-16 border-1 border-[#F3F1F6] mb-2" />
+            <span className="text-center font-bold mt-2 font-be-vietnam text-[#F3F1F6]">
+              {extra2}
             </span>
           </div>
         </div>
